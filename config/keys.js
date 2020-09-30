754efe06f2c758ff30d69f0b7e0da73716a6126d
WasyfAli <46135061+WasyfAli@users.noreply.github.com>
2020-09-30 16:54:50 +0530

    update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@ module.exports = {
   client: "mysql",
   connection: {
     host: "localhost",
-    user: "YOUR_DB_USERNAME",
-    database: "YOUR_DB_NAME",
-    password: "YOUR_DB_PASSWORD",
+    user: "root",
+    database: "test",
+    password: "redhat",
   },
 };
